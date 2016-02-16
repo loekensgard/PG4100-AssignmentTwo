3c43aadedd8352fffb7781a09031d1406786e36c
Thorstein <loktho14@student.westerdals.no>
2016-02-16 11:36:38 +0100

    Fixxxxxxxxxxxxxxxxxx

--- src/main/java/no/westerdals/student/loktho14/PG4100/socketInnlevering/Client.java
+++ src/main/java/no/westerdals/student/loktho14/PG4100/socketInnlevering/Client.java
@@ -1,55 +1,57 @@
 package no.westerdals.student.loktho14.PG4100.socketInnlevering;
 
+import java.io.*;
+import java.net.*;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.Socket;
-import java.util.Scanner;
-
-/**
- * Created by Thorstein on 15.02.2016.
- */
 public class Client {
-    private final String ADRESSE = "localhost";
-    private final int PORT = 5632;
-    private boolean running = true;
-    private final Scanner SCANNER = new Scanner(System.in);
-
-    public Client(){
-        try{
-
-        Socket clientSocket = new Socket(ADRESSE, PORT);
-        DataOutputStream outputStream = new DataOutputStream(clientSocket.getOutputStream());
-        DataInputStream inputStream = new DataInputStream(clientSocket.getInputStream());
+    private static String serverHost = "localhost";
+    private static PrintWriter out = null;
+    private static BufferedReader in = null;
+    private static Socket socket = null;
+    private static BufferedReader stdIn;
 
 
-            while(running){
-                if(SCANNER.hasNextLine()){
-                    String text = SCANNER.nextLine();
-                    outputStream.writeUTF(text);
+    public static void main(String[] args) throws IOException {
 
-                    if(text.toLowerCase().equals("nei")){
-                        running = false;
-                    }
-                }
-            }
+        String serverHostname = serverHost;
 
-
-            while(running) {
-                System.out.println(inputStream.readUTF());
-            }
-
-
-        }catch (IOException e){
-            e.printStackTrace();
+        if (args.length > 0) {
+            serverHostname = args[0];
         }
 
+        System.out.println("Prøver å koble til host " +
+                serverHostname + " on port 5555.");
+
+        try {
+            socket = new Socket(serverHostname, 5555);
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (UnknownHostException e) {
+            System.err.println("Finner ikke hosten: " + serverHostname);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Serveren er ikke oppe å kjører: " + serverHostname);
+            System.exit(1);
+        }
+
+        stdIn = new BufferedReader(new InputStreamReader(System.in));
+        String userInput;
+
+        System.out.println("Skriv (\"Nei\") for å avslutte");
+        while ((userInput = stdIn.readLine()) != null) {
+            out.println(userInput);
+
+            // end loop
+            if (userInput.toLowerCase().equals("nei")) {
+                break;
+            }
+
+            System.out.println("echo: " + in.readLine());
+        }
+
+        out.close();
+        in.close();
+        stdIn.close();
+        socket.close();
     }
-
-
-    public static void main(String[] args){
-        new Client();
-    }
-
 }
--- src/main/java/no/westerdals/student/loktho14/PG4100/socketInnlevering/Server.java
+++ src/main/java/no/westerdals/student/loktho14/PG4100/socketInnlevering/Server.java
@@ -1,28 +1,80 @@
 package no.westerdals.student.loktho14.PG4100.socketInnlevering;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
+import java.io.*;
 
-/**
- * Created by Thorstein on 15.02.2016.
- */
-public class Server {
-    private final int port = 5632;
+public class Server extends Thread {
+    private static boolean serverRunning = true;
+    private Socket clientSocket;
+    private static final int PORT = 5555;
+    private PrintWriter out;
+    private BufferedReader in;
 
-    public Server(){
-        try{
-            ServerSocket serverConnection = new ServerSocket(port);
-            while(true){
-                Socket socket = serverConnection.accept();
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+
+        try {
+            serverSocket = new ServerSocket(PORT);
+            System.out.println("Socket er laget");
+            try {
+                while (serverRunning) {
+                    new Server(serverSocket.accept());
+                }
+            } catch (IOException e) {
+                System.err.println("Klarte ikke ta imot client");
+                System.exit(1);
             }
-        }catch (IOException e){
-            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Kunne ikke lytte på port 5555");
+            System.exit(1);
+        } finally {
+            try {
+                System.out.println("Lukker connectionen");
+                serverSocket.close();
+            } catch (IOException e) {
+                System.err.println("Klarte ikke lukke port 5555.");
+                System.exit(1);
+            }
         }
     }
 
-    public static void main(String [] args){
-        new Server();
+    private Server(Socket clientSoc) {
+        clientSocket = clientSoc;
+        start();
     }
 
+    public void run() {
+
+        System.out.println("Ny client er koblet til");
+
+
+        try {
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("Server: " + inputLine);
+
+                if (inputLine.equals("?")) {
+                    inputLine = new String("\"Nei\" ends Client, ");
+                }
+                out.println(inputLine);
+                out.flush();
+
+                if (inputLine.toLowerCase().equals("nei")) {
+                    break;
+                }
+            }
+
+            out.close();
+            in.close();
+            clientSocket.close();
+        } catch (IOException e) {
+            System.err.println("Problem med sending av tekst");
+            System.exit(1);
+        }
+    }
 }
